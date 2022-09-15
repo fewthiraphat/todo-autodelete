bf312172ff2faa643663a236bc2b6909c3590ccb
Thiraphat <few.thiraphat@gmail.com>
2022-09-15 16:04:43 +0700

    refactor for clean code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import './App.css';
-import Task from './components/Task';
-import {useState} from 'react'
+import Task from './containers/Task';
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [task , setTask] = useState('');
-  
-  const addTask = () => {
-    setTasks([...tasks, task]);
-    setTask('');
-  }
-  const handleChangeTask = (e) => {
-    setTask(e.target.value);
-  }
 
   return (
-    <div className="App">
-      <input type="text" value={task} onChange={handleChangeTask}/>
-      <button onClick={addTask}>Enter</button>
-      {
-        tasks.map(task => <Task message={task}/>)
-      }
-    </div>
+      <Task/>
   );
 }
 
---
+++ src/containers/Task.js
@@ -0,0 +1,26 @@
+import Task from '../components/Task';
+import { useState } from 'react'
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [task , setTask] = useState('');
+  
+  const addTask = () => {
+    setTasks([...tasks, task]);
+    setTask('');
+  }
+  const handleChangeTask = (e) => {
+    setTask(e.target.value);
+  }
+
+  return (
+    <div className="App">
+      <input type="text" value={task} onChange={handleChangeTask}/>
+      <button onClick={addTask}>Enter</button>
+      {
+        tasks.map(task => <Task message={task}/>)
+      }
+    </div>
+  );
+}
+
+export default App;
